3676b12aa48bae51cfa43b9496a8351fac991443
Martin Zimmer <martinzimmer@Martins-Air.fritz.box>
2020-11-17 01:24:30 +0100

    0.0.9.5

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4dfee03b35406082e777";
+/******/ 	var hotCurrentHash = "c17077dde2dfa7ec4819";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
